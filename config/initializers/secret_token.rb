600515b7338bbe92ae5c6045c3bb2c959c9da6ff
unknown <User@.(none)>
2012-05-13 10:53:19 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiLord::Application.config.secret_token = 'd7b0987f5fd3675f791fec34e77c0489c6ae14da5b99045ea252b46726248258e9aa006ef8420057a65d018ec7900562e1f0bb178fe07935f88a8b56cc594dfd'
